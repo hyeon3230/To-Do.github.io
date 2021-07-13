a6ed2714e232b3e4ed24e5a892c2238a993917fd
hyeon3230 <86290231+hyeon3230@users.noreply.github.com>
2021-07-14 00:58:38 +0900

    Greetings according to time.
    I need to figure out a way to summarize the code.

--- login.js
+++ login.js
@@ -16,8 +16,25 @@ function loginSubmit(event){
   paintGreeting(username);
 }
 
+const time = new Date();
+const nowHour = time.getHours();
+
+function nowTime(username){
+  if (24 <= nowHour < 6){
+      greeting.innerText = `Good dawn, ${username}`;
+  } else if (6 <= nowHour < 12){
+    greeting.innerText = `Good morning, ${username}`;
+  } else if (12 <= nowHour < 5){
+    greeting.innerText = `Good afternoon, ${username}`;
+  } else if (5 <= nowHour < 8){
+    greeting.innerText = `Good evening, ${username}`;
+  } else if (8 <= nowHour < 12){
+    greeting.innerText = `Good night, ${username}`;
+  }
+}
+
 function paintGreeting(username){
-  greeting.innerText = `Hello ${username}`;
+  nowTime(username);
   greeting.classList.remove("hidden");
 }
 
