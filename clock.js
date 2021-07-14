5cfc8bfe29e8ba9919786139e75a888561cf1f3a
hyeon3230 <86290231+hyeon3230@users.noreply.github.com>
2021-07-14 21:29:40 +0900

    Update clock.js

--- clock.js
+++ clock.js
@@ -4,7 +4,8 @@ function getTime(){
   const date = new Date();
   const hours = String(date.getHours()).padStart(2, "0");
   const minutes = String(date.getMinutes()).padStart(2, "0");
-  clock.innerText = `${hours}:${minutes}`;
+  const seconds = String(date.getSeconds()).padStart(2, "0");
+  clock.innerText = `${hours}:${minutes}:${seconds}`;
 }
 
 getTime();
